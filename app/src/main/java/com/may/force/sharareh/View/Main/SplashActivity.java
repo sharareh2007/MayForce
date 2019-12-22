c474778c341fe742641b9501e2f2434f8a427e5e
sparky2018 <sharareh.monfared@tekever.com>
2019-12-22 19:52:54 +0000

    Added SplashScreen and Icon logo.

---
+++ app/src/main/java/com/may/force/sharareh/View/Main/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.may.force.sharareh.View.Main;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+    }
+}
